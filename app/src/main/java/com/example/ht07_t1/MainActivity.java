57c9fa50dd7b5f530639a4a9e0f28555336c00e4
MaHe-byte <markus.heikkila@live.com>
2021-03-08 17:52:18 +0200

    HT07-03

--- app/src/main/java/com/example/ht07_t1/MainActivity.java
+++ app/src/main/java/com/example/ht07_t1/MainActivity.java
@@ -4,20 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView text;
+    EditText inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView);
+        inputText = findViewById(R.id.editTextTextPersonName);
 
     }
 
     public void printFunction(View v){
         System.out.println("Hello World!");
-
+        text.setText(inputText.getText().toString());
     }
 }

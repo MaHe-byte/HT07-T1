4e096e02f76cdd2dee70483a0168a11a36fd453c
MaHe-byte <markus.heikkila@live.com>
2021-03-08 17:23:19 +0200

    HT07-01

--- app/src/main/java/com/example/ht07_t1/MainActivity.java
+++ app/src/main/java/com/example/ht07_t1/MainActivity.java
@@ -8,18 +8,16 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        text = findViewById(R.id.textView);
+
 
     }
 
     public void printFunction(View v){
         System.out.println("Hello World!");
-        text.setText("Hello World!");
+
     }
 }
